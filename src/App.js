f97670718bbbef6d211d2ac102274060ad8bc433
jbrown7407 <jbrown7407@gmail.com>
2021-01-07 18:10:06 -0500

    API call for coins/markets

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react'
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+// import Coin from './Coin';
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Crypto Tracker</h1>
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
-      <h2> App</h2>
-      <h2> Search</h2>
-      <h2> Text </h2>
-      <h2> Input</h2>
-    </div>
-  );
-}
-
-export default App;
+  useEffect(() => {
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
+  }, []);
