f89695dc0898b7e56710c62391eb2c06520c87bf
jbrown7407 <jbrown7407@gmail.com>
2021-01-08 01:18:48 -0500

    label row

--- src/App.js
+++ src/App.js
@@ -48,7 +48,28 @@ function App() {
       {filteredCoins.map(coin => {
         return (
           <div>
+          <div className='coin-row'>
+        <div className='coin'>
+          <h1>img </h1>
+          <h1>Name</h1>
+          <p className='coin-symbol'>Symbol</p>
+        </div>
+        <div className='coin-data'>
+          <p className='coin-price'>Price</p>
+          <p className='coin-ath'>ATH</p>
+          <p className='coin-volume'>Vol</p>
+
+          
+            <p className='coin-percent red'>Price Change</p>
          
+            {/* <p className='coin-percent green'>+{priceChange.toFixed(2)}%</p> */}
+          
+
+          <p className='coin-marketcap'>
+            Mkt Cap
+          </p>
+        </div>
+      </div>
           <Coin
             key={coin.id}
             name={coin.name}
--- src/Coin.js
+++ src/Coin.js
@@ -13,6 +13,10 @@ const Coin = ({
   
 }) => {
   return (
+<>
+   
+
+
     <div className='coin-container'>
       <div className='coin-row'>
         <div className='coin'>
@@ -21,14 +25,14 @@ const Coin = ({
           <p className='coin-symbol'>{symbol}</p>
         </div>
         <div className='coin-data'>
-          <p className='coin-price'>${price}</p>
-          <p className='coin-ath'>${ath}</p>
-          <p className='coin-volume'>${volume.toLocaleString()}</p>
+          <p className='coin-price'>Price: ${price}</p>
+          <p className='coin-ath'>ATH: ${ath}</p>
+          <p className='coin-volume'>Vol: ${volume.toLocaleString()}</p>
 
           {priceChange < 0 ? (
-            <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+            <p className='coin-percent red'>-{priceChange.toFixed(2)}%</p>
           ) : (
-            <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+            <p className='coin-percent green'>+{priceChange.toFixed(2)}%</p>
           )}
 
           <p className='coin-marketcap'>
@@ -37,6 +41,7 @@ const Coin = ({
         </div>
       </div>
     </div>
+    </>
   );
 };
 
